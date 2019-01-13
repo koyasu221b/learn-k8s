805d67a057a776fa5581dffc5bd89852788630ce
Will Huang <will_huang@trend.com.tw>
2019-01-13 15:50:30 +0800

    updte client

--- complex/client/src/OtherPage.js
+++ complex/client/src/OtherPage.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-
-export  default () => {
-    return (
-        <div>
-            Im some other page!
-            <Link to="/"/>Go back hom</Link>
-        </div>
-    );
+export default () => {
+  return (
+    <div>
+      Im some other page
+      <Link to="/">Go back to home page!</Link>
+    </div>
+  );
 };
